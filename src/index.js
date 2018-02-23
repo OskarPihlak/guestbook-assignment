a7a190643a747c8a20116309d6a1927e528fba0c
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-02-23 02:32:32 +0200

    guestbook-assignment with passing tests

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ip from 'ip';
 
 const moment = require('moment');
 const loki = require('lokijs');
+const color = require('colors');
 
-//database
+//database declaration
 let db = new loki('loki.json');
 let data = db.addCollection('data', {indices: ['id'], unique: ['id']});
 data.ensureUniqueIndex('id');
@@ -14,6 +15,18 @@ data.ensureUniqueIndex('id');
 const app = restify.createServer({});
 const cors = corsMiddleware({});
 
+//database helper
+function loadCollection(colName, callback) {
+    db.loadDatabase({}, () => {
+        let _collection = db.getCollection(colName);
+        if (!_collection) {
+            console.log("Collection %s does not exit. Creating ...", colName);
+            _collection = db.addCollection('users');
+        }
+        callback(_collection);
+    });
+}
+
 //middleware
 app.pre(cors.preflight);
 app.use(cors.actual);
@@ -21,46 +34,53 @@ app.use(restify.plugins.bodyParser({mapParams: false}));
 
 app.get('/guestbook', (req, res) => {
     res.header("Content-Type", 'application/json');
-    db.loadDatabase({}, () => {
-        let all_data = db.getCollection('data');
-        let data = all_data.find({});
-        res.status(200);
-        res.json({data});
+    let data = [];
+    loadCollection('data', collection => {
+        let col = collection.find({});
+        col.forEach(obj => {
+            if (!obj.hasOwnProperty('deleted_at')) data.push(obj);
+        });
+        if (data.length > 0) {
+            res.status(200);
+            res.json({data});
+        } else {
+            res.status(404);
+            res.json({message: "No results returned from database"});
+        }
     });
 });
 
 app.post('/guestbook', (req, res) => {
     let new_guestbook_object = {
-        first_name: req.body.first_name,    //first_name
-        last_name: req.body.last_name,     //last_name
-        message: req.body.message,       //message
-        created_at: moment().toISOString()  //created
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        message: req.body.message,
+        created_at: moment().toISOString()
     };
+    loadCollection('data', data => {
+        let resultObj = data.insert(new_guestbook_object);
+        resultObj.id = resultObj.$loki;
+        data.update(resultObj);
+        db.save();
 
-    let resultObj = data.insert(new_guestbook_object);
-    resultObj.id = resultObj.$loki;
-    data.update(resultObj);
-    db.save();
-
-    console.log(resultObj);
-
-    res.status(201);
-    res.send({
-        id: new_guestbook_object.id,
-        created_at: new_guestbook_object.created_at
+        console.log(resultObj);
+        res.status(201);
+        res.send({
+            data: {
+                id: new_guestbook_object.id,
+                created_at: new_guestbook_object.created_at
+            }
+        });
     });
 });
 
 app.get('/guestbook/:id', (req, res) => {
     res.header("Content-Type", 'application/json');
-    db.loadDatabase({}, () => {
-        let all_data = db.getCollection('data');
-        let result = all_data.findOne({id: parseInt(req.params.id)});
+    loadCollection('data', data => {
+        let result = data.findOne({id: parseInt(req.params.id)});
 
-        console.log(result !== null && result.hasOwnProperty('deleted_at') === false);
+        console.log(color.green(`\n Requested id: ${req.params.id} \n`), result);
         if (result !== null && result.hasOwnProperty('deleted_at') === false) {
-            console.log(result);
-            console.log(result.hasOwnProperty('deleted_at') === false);
             res.status(200);
             res.send(
                 {
@@ -73,23 +93,21 @@ app.get('/guestbook/:id', (req, res) => {
                     }
                 }
             );
-
         } else if (result === null || result.hasOwnProperty('deleted_at') === true) {
             res.status(404);
-            res.send({message: "Not Found"});
+            res.send({message: "Entry not found"});
         }
     });
 });
 
 app.del('/guestbook/:id', (req, res) => {
     res.header("Content-Type", 'application/json');
-    db.loadDatabase({}, () => {
-        let all_data = db.getCollection('data');
-        let requested_delete = all_data.findOne({id: parseInt(req.params.id)});
+    loadCollection('data', collection => {
+        let requested_delete = collection.findOne({id: parseInt(req.params.id)});
 
         if (requested_delete !== null) {
             requested_delete.deleted_at = moment().toISOString();
-            all_data.update(requested_delete);
+            collection.update(requested_delete);
             db.save();
             res.status(200);
             res.send({
@@ -98,15 +116,12 @@ app.del('/guestbook/:id', (req, res) => {
                     deleted_at: requested_delete
                 }
             });
-
         } else if (requested_delete === null) {
-            res.header("Content-Type", 'application/json');
             res.status(404);
-            res.send({message: "Not Found"});
+            res.send({data: {message: "Entry not found"}});
         }
     });
 });
-
 
 const port = (process.env.PORT) || 3000;
 app.listen(port, () => {
